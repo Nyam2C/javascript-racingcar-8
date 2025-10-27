087e836ce8c7d17ed8b459047580c4ef58d47981
Nyam2C <qkrxogus113@naver.com>
2025-10-27 11:32:19 +0900

    feat: 순차적으로 횟수만큼 이동

--- src/controller/RacingController.js
+++ src/controller/RacingController.js
@@ -2,7 +2,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 import { InputView } from "../views/index.js";
 import { OutputView } from '../views/index.js';
 import { validateCarNameArray, validateTryCount } from '../utils/index.js';
-
+import { Race } from "../models/index.js";
 
 export class RacingController {
     async run() {
@@ -11,9 +11,21 @@ export class RacingController {
             validateCarNameArray(carNames);
             const tryCount = await InputView.readTryCount();
             validateTryCount(tryCount);
+
+            const race = new Race(carNames, tryCount);
+
+            OutputView.printStart();
+
+
         } catch(error) {
             OutputView.printError(error.message);
             throw error;
+        }
+    }
+
+    startRace() {
+        for (let i = 0; i < this.tryCount; i++) {
+            this.moveCars();
         }
     }
 }
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,6 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { PROMPT } from "../constants/index.js";
 
 const OutputView = {
+
+    printStart() {
+        MissionUtils.Console.print(PROMPT.RACE_START);
+    },
+
     printError(errorMessage) {
         MissionUtils.Console.print(errorMessage);
     },
